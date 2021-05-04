856aa14c16bb2f2fbc15ac1d67729bdd055a23cb
Gourab Nag <gourabnag12@gmail.com>
2021-05-04 19:59:57 +0530

    finishing up

--- src/pupeteer-invoice.js
+++ src/pupeteer-invoice.js
@@ -322,36 +322,8 @@ async function generateInvoice({
   // Cleanup
   await browser.close();
 
-  // End
+  // Ends
   return createdPath;
 }
-
-const data = [
-  {
-    ID: 0,
-    Name: "boq_item_1",
-    Description: "This is the item description",
-    Unit: "grams",
-    Make: "NA",
-    Quantity: 10,
-    Price: 180,
-    Total: 1800,
-  },
-  {
-    ID: 1,
-    Name: "boq_item_9",
-    Description: "This is the item description",
-    Unit: "grams",
-    Make: "NA",
-    Quantity: 90,
-    Price: 900,
-    Total: 81000,
-  },
-];
-const table = {
-  headers: Object.keys(data[0]),
-  rows: data.map((row) => Object.values(row)),
-};
-generateInvoice({ table });
 
 module.exports = generateInvoice;
